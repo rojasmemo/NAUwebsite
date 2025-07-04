11560e9cffa02ad0d40c2940c421987941914f2f
rojasmemo <rojas.memo@gmail.com>
2025-07-04 17:02:15 -0500

    Claves protegidas con var de ambiente

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,6 @@
 // Importa las funciones y módulos necesarios de Vite y Node.js
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { readdirSync } from 'fs';
 import handlebars from 'vite-plugin-handlebars';
 
@@ -25,21 +25,32 @@ const getHtmlInput = () => {
 };
 
 // La función defineConfig ofrece autocompletado y validación para tu configuración.
-export default defineConfig({
-  // Para desplegar en un dominio raíz (Netlify, GoDaddy), la base debe ser '/'.
-  base: '/',
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno del archivo .env según el modo (development o production)
+  const env = loadEnv(mode, projectRoot, '');
 
-  // Configuración para el proceso de compilación (build).
-  build: {
-    // Usamos la función para generar las entradas dinámicamente.
-    rollupOptions: { input: getHtmlInput() },
-    // Define el directorio de salida para los archivos compilados.
-    outDir: 'dist',
-  },
+  return {
+    // Para desplegar en un dominio raíz (Netlify, GoDaddy), la base debe ser '/'.
+    base: '/',
 
-  plugins: [
-    handlebars({
-      partialDirectory: resolve(projectRoot, 'partials'),
-    }),
-  ],
+    // Pone las variables de entorno a disposición del código del lado del cliente.
+    define: {
+      'process.env.VITE_RECAPTCHA_SITE_KEY': JSON.stringify(env.VITE_RECAPTCHA_SITE_KEY),
+      'process.env.VITE_RECAPTCHA_SECRET_KEY': JSON.stringify(env.VITE_RECAPTCHA_SECRET_KEY)
+    },
+
+    // Configuración para el proceso de compilación (build).
+    build: {
+      // Usamos la función para generar las entradas dinámicamente.
+      rollupOptions: { input: getHtmlInput() },
+      // Define el directorio de salida para los archivos compilados.
+      outDir: 'dist',
+    },
+
+    plugins: [
+      handlebars({
+        partialDirectory: resolve(projectRoot, 'partials'),
+      }),
+    ],
+  };
 });
